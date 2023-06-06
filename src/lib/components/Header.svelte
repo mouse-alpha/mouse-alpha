0e91baace789298f779d7d707221e33ae1f051b0
mouse <narek.kostanyan@inspace.chat>
2023-06-06 17:26:07 +0400

    add default style

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,50 @@
+<script>
+	/**
+	 * @type {string}
+	 */
+	export let title;
+	import HomeIcon from '$lib/icons/home.svelte';
+	import RssIcon from '$lib/icons/rss.svelte';
+	import '$lib/styles/style.css';
+</script>
+
+<header>
+	<div class="title" data-testid="title">{title}</div>
+
+	<nav>
+		<ul>
+			<li>
+				<a href="/" data-testid="home-link"><HomeIcon /></a>
+			</li>
+			<li>
+				<a href="/" data-testid="rss-link"><RssIcon /></a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		padding: 0.2rem;
+		color: var(--secondory-color);
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		text-align: center;
+		overflow: hidden;
+		border-bottom: 1px solid var(--primary-color);
+	}
+
+	header .title {
+		font-size: 1.6rem;
+		text-align: left;
+	}
+
+	ul {
+		margin: 0;
+		list-style-type: none;
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import '$lib/styles/style.css';
+</script>
+
+<div class="container">
+	<Header title="Narek Kostanyan's Blog" />
+	<main>
+		<slot />
+	</main>
+</div>
+<Footer />
