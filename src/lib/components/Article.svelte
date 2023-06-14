c233ca8f6a909f2f96ebf7d35cf1b59d85e34cd2
mouse <narek.kostanyan@inspace.chat>
2023-06-14 15:59:21 +0400

    add theme

---
+++ src/lib/components/Article.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { Article } from '$lib/types/Article';
+	export let article: Article;
+</script>
+
+<div class="article">
+	<div class="title">{article.title}</div>
+	<div class="publish-date">{article.publishDate.toDateString()}</div>
+	<div class="details">{article.description}<a href="/">Click here to read more.</a></div>
+</div>
+
+<style>
+	.article {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		padding: 16px;
+		max-width: 360px;
+	}
+
+	.title {
+		font-size: 24px;
+		font-weight: 500;
+	}
+
+	.publish-date {
+		font-size: 12px;
+		font-weight: 200;
+	}
+
+	.details {
+		font-size: 18px;
+		font-weight: 400;
+	}
+
+	a {
+		margin-left: 6px;
+		font-size: 12px;
+		font-weight: 600;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,29 @@
-<script>
-	import Footer from '$lib/components/Footer.svelte';
+<script lang="ts">
 	import Header from '$lib/components/Header.svelte';
+
 	import '$lib/styles/style.css';
+
+	const tags = [
+		'golang',
+		'testing',
+		'playwright',
+		'typescript',
+		'python',
+		'photo',
+		'animals',
+		'aquascapes'
+	];
+	const socialLinks = {
+		linkedin: 'https://linkedin.com',
+		github: 'https://github.com',
+		email: 'mail@example.com'
+	};
+	const ownerName = 'Narek Kostanyan';
 </script>
 
-<div class="container">
-	<Header title="Narek Kostanyan's Blog" />
+<Header {ownerName} {tags} {socialLinks} />
+<div class="content">
 	<main>
 		<slot />
 	</main>
 </div>
-<Footer />
