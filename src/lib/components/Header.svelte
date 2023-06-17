635df55b023158b20fe5f8f8515244ecd03e106b
Mouse* <narek.mouse@gmail.com>
2023-06-17 17:54:12 +0400

    betta label (#16)
    Co-authored-by: mouse <narek.kostanyan@inspace.chat>

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="header">
-	<div class="blog-title">Welcome to<br /> {ownerName}'s <br />Blog</div>
+	<div class="blog-title">Welcome to<br /> {ownerName}'s <br />Blog <div class='betta'> Betta - v0.2.3</div> </div>
 	<Navigation {socialLinks} />
 	<Tags {tags} />
 </div>
@@ -28,5 +28,16 @@
 		font-weight: 500;
 		font-size: 36px;
 		padding-bottom: 10px;
+	}
+
+	.betta {
+		text-align: center;
+		font-size: 12px;
+		font-weight: 900;
+		padding: 6px 12px;
+		background-color: #b00020;
+		width:fit-content;
+		border-radius: 12px;
+		margin-top: 6px;
 	}
 </style>
